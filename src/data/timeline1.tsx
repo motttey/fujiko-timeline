ad7120314b5471e7e6a53db5e2e0ea92877e878e
motttey <motitago@gmail.com>
2025-07-06 17:40:17 +0900

    modify: externalize data

---
+++ src/data/timeline1.tsx
@@ -0,0 +1,93 @@
+export type TimelineItem = {
+  id: number;
+  date: string;
+  work: string;
+  url: string;
+};
+
+export const timelineData: TimelineItem[] = [
+  {
+    id: 1,
+    date: "1983-05-28",
+    work: "てんコミ大長編鬼岩城",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1918816524514505130",
+  },
+  {
+    id: 2,
+    date: "1983-05-28",
+    work: "てんコミ大長編鬼岩城",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1941308279805931838",
+  },
+  {
+    id: 3,
+    date: "1983-05-28",
+    work: "てんコミ大長編鬼岩城",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1940938557465809207",
+  },
+  {
+    id: 4,
+    date: "1983-05-28",
+    work: "てんコミ大長編鬼岩城",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1940583974541447186",
+  },
+  {
+    id: 5,
+    date: "1983-05-28",
+    work: "てんコミ大長編鬼岩城",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1939864099556663519",
+  },
+  {
+    id: 6,
+    date: "1976-09-01",
+    work: "みどりの守り神",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1909901401742029056",
+  },
+  {
+    id: 7,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1906968694099480962",
+  },
+  {
+    id: 8,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1907307948478746669",
+  },
+  {
+    id: 9,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1907638029260566923",
+  },
+  {
+    id: 10,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1908003288899027034",
+  },
+  {
+    id: 11,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1908385657317462179",
+  },
+  {
+    id: 12,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1908815254940966991",
+  },
+    {
+    id: 13,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1909112837617914071",
+  },
+  {
+    id: 14,
+    date: "1985-08-01",
+    work: "アン子大いに怒る",
+    url: "https://twitter.com/uBTpfEAU2cEZucx/status/1909503047010750781",
+  },
+];
